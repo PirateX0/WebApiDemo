ea5096d3dcdcbc0eb2c73cb3a24c0b1537d2ca51
PirateX0 <295528322@qq.com>
2018-10-04 13:45:11 +0800

    Add project files.

---
+++ GenerateSqlDbFromEf/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserCenter.Services;
+using UserCenter.Services.Models;
+
+namespace GenerateSqlDbFromEf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UCDbContext ctx = new UCDbContext())
+            {
+
+                User user = new User();
+                user.NickName = "dalong";
+                user.PhoneNum = "123";
+                user.PasswordHash = "123";
+                user.PasswordSalt = "123";
+
+                ctx.Users.Add(user);
+                 ctx.SaveChanges();
+            }
+
+            Console.WriteLine("ok");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UserCenter.OpenAPI/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using UserCenter.IServices;
+using UserCenter.OpenAPI.Filters;
+
+namespace UserCenter.OpenAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            InitAutoFac();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);            
+        }
+
+        private void InitAutoFac()
+        {
+            var configuration = GlobalConfiguration.Configuration;
+            var builder = new ContainerBuilder();
+            // Register API controllers using assembly scanning.
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()).PropertiesAutowired();
+
+            builder.RegisterWebApiFilterProvider(configuration);
+            
+            var services = Assembly.Load("UserCenter.Services");
+            builder.RegisterAssemblyTypes(services)
+            .Where(type => !type.IsAbstract && typeof(IServiceTag).IsAssignableFrom(type))
+            .AsImplementedInterfaces().SingleInstance().PropertiesAutowired();
+
+            builder.RegisterType(typeof(UCAuthorizationFilter)).PropertiesAutowired();
+
+            var container = builder.Build();
+            // Set the WebApi dependency resolver.
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            configuration.DependencyResolver = resolver;
+        }
+
+    }
+}
